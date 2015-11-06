0e13da50ffee75d4de9defb1908383108cdbbaab
magentoguys <christie.michael@gmail.com>
2015-11-06 04:47:37 -0500

    change for resolve secure url issue on live site when add to cart from wishlist by magentoguys dev

--- app/code/local/Magentoguys/Wishlistreminder/controllers/IndexController.php
+++ app/code/local/Magentoguys/Wishlistreminder/controllers/IndexController.php
@@ -220,5 +220,105 @@ class Magentoguys_Wishlistreminder_IndexController extends Mage_Wishlist_IndexCo
         }
     }
 
+	/**
+     * Add wishlist item to shopping cart and remove from wishlist
+     *
+     * If Product has required options - item removed from wishlist and redirect
+     * to product view page with message about needed defined required options
+     */
+    public function cartAction()
+    {
+        if (!$this->_validateFormKey()) {
+            return $this->_redirect('*/*');
+        }
+        $itemId = (int) $this->getRequest()->getParam('item');
+
+        /* @var $item Mage_Wishlist_Model_Item */
+        $item = Mage::getModel('wishlist/item')->load($itemId);
+        if (!$item->getId()) {
+            return $this->_redirect('*/*');
+        }
+        $wishlist = $this->_getWishlist($item->getWishlistId());
+        if (!$wishlist) {
+            return $this->_redirect('*/*');
+        }
+
+        // Set qty
+        $qty = $this->getRequest()->getParam('qty');
+        if (is_array($qty)) {
+            if (isset($qty[$itemId])) {
+                $qty = $qty[$itemId];
+            } else {
+                $qty = 1;
+            }
+        }
+        $qty = $this->_processLocalizedQty($qty);
+        if ($qty) {
+            $item->setQty($qty);
+        }
+
+        /* @var $session Mage_Wishlist_Model_Session */
+        $session    = Mage::getSingleton('wishlist/session');
+        $cart       = Mage::getSingleton('checkout/cart');
+
+        $redirectUrl = Mage::getUrl('*/*');
+
+        try {
+            $options = Mage::getModel('wishlist/item_option')->getCollection()
+                    ->addItemFilter(array($itemId));
+            $item->setOptions($options->getOptionsByItem($itemId));
+
+            $buyRequest = Mage::helper('catalog/product')->addParamsToBuyRequest(
+                $this->getRequest()->getParams(),
+                array('current_config' => $item->getBuyRequest())
+            );
+
+            $item->mergeBuyRequest($buyRequest);
+            if ($item->addToCart($cart, true)) {
+                $cart->save()->getQuote()->collectTotals();
+            }
+
+            $wishlist->save();
+            Mage::helper('wishlist')->calculate();
+
+			/***code modified by mgguys for secure url issue on wishlist***/
+            if (Mage::helper('checkout/cart')->getShouldRedirectToCart()) {
+				if(isSecureUrl==1){
+					$redirectUrl = Mage::helper('checkout/cart')->getCartUrl();
+					$redirectUrl = str_replace("http://", "https://",$redirectUrl);
+				}elseif(isSecureUrl==0){
+					$redirectUrl = Mage::helper('checkout/cart')->getCartUrl();
+				}
+			}
+			/*****************************/
+			
+			Mage::helper('wishlist')->calculate();
+
+            $product = Mage::getModel('catalog/product')
+                ->setStoreId(Mage::app()->getStore()->getId())
+                ->load($item->getProductId());
+            $productName = Mage::helper('core')->escapeHtml($product->getName());
+            $message = $this->__('%s was added to your shopping cart.', $productName);
+            Mage::getSingleton('catalog/session')->addSuccess($message);
+        } catch (Mage_Core_Exception $e) {
+            if ($e->getCode() == Mage_Wishlist_Model_Item::EXCEPTION_CODE_NOT_SALABLE) {
+                $session->addError($this->__('This product(s) is currently out of stock'));
+            } else if ($e->getCode() == Mage_Wishlist_Model_Item::EXCEPTION_CODE_HAS_REQUIRED_OPTIONS) {
+                Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
+                $redirectUrl = Mage::getUrl('*/*/configure/', array('id' => $item->getId()));
+            } else {
+                Mage::getSingleton('catalog/session')->addNotice($e->getMessage());
+                $redirectUrl = Mage::getUrl('*/*/configure/', array('id' => $item->getId()));
+            }
+        } catch (Exception $e) {
+            Mage::logException($e);
+            $session->addException($e, $this->__('Cannot add item to shopping cart'));
+        }
+
+        Mage::helper('wishlist')->calculate();
+
+        return $this->_redirectUrl($redirectUrl);
+    }
+
 
 }
